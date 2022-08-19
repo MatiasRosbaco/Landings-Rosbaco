a82aeb0c88df2a0c8dc469aefde6c8cdb9d647ae
MatiasRosbaco <mati.rosbaco@gmail.com>
2022-08-19 14:30:59 -0300

    moodInversor ya funcionando

--- Landings/routes/index.js
+++ Landings/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+router.get('/Rosbaco/moodInversor', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../views/moodinversor.html'));
 });
 
 module.exports = router;
