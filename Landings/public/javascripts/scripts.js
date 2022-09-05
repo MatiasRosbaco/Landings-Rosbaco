8b4bd867ca7ff75f2e125fa56e1490534ff0a519
MatiasRosbaco <mati.rosbaco@gmail.com>
2022-09-05 12:16:03 -0300

    agregue version con formulario

--- Landings/public/javascripts/scripts.js
+++ Landings/public/javascripts/scripts.js
@@ -23,3 +23,82 @@ const swiper = new Swiper('.swiper', {
     $('.modal').modal();
 
   });
+  $( "#datepicker" ).datepicker({ dateFormat: 'dd/mm/yy' });
+
+  $('#agente').change(function () {
+    if($(this).hasClass('active')){
+      $('.daySelect').hide();
+      $(this).removeClass('active');
+    }else{
+      $(this).addClass('active');
+      $('.daySelect').show();
+    }
+ });
+
+
+
+
+
+$('input, textarea').on('change', function(){
+  var nombre = $('#name').val(),
+  email = $('#email').val(),
+  telefono = $('#tel').val(),
+  fecha = $('#datepicker').val(),
+  mensaje = $('#message').val();
+
+
+  if(nombre != '' && email != '' && telefono != '' && mensaje != ''){
+    $( "form button" ).prop( "disabled", false );
+  }
+});
+
+
+$('form').on('submit', function(e){
+  $('.loading').show();
+  e.preventDefault();
+
+  var nombre = $('#name').val(),
+  email = $('#email').val(),
+  telefono = $('#tel').val(),
+  fecha = $('#datepicker').val(),
+  mensaje = $('#message').val();
+var data = {
+  "doNotCall": false,
+  "email": email,
+  "fechaEntrevista": fecha,
+  "lastName": nombre,
+  "leadSource": nombre,
+  "mensajeWeb": mensaje,
+  "name": nombre,
+  "phone": telefono,
+  "preferenciaHorarioContactacion": "8 a 12",
+  "utm_source": "fb",
+  "utm_medium": "Facebook_Right_Column",
+  "utm_campaign":"Campaña_de_Prueba",
+  "utm_content": "Coliving_Prueba",
+  "utm_segmento": "INVERSOR-PRUEBA-REAL_ESTATE",
+  "utm_edad" : "40",
+  "utm_placement": "prueba placement1",
+  "utm_zona": "prueba zona 1",
+  "utm": "utm prueba 1",
+  "proyectoInteresManual": "General"
+}
+
+
+var ajaxurl = 'https://rosbaco-backend.herokuapp.com/landing/lead';
+$.post({
+  url: ajaxurl,
+  type: "POST",
+  headers:{"content-type":"application/json"},
+  dataType: 'json',
+  data: JSON.stringify(data)
+})
+.done((data, status) => {
+    $('.loading').hide();
+    console.log(data, status);
+
+
+});
+
+
+});
